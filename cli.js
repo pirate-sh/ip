a1c3c609fb1b0ec3aefd8cc4781d4746519cb381
treehouse-su <stefan@unterhauser.name>
2015-04-08 22:37:55 +0000

    back to default added

--- cli.js
+++ cli.js
@@ -5,7 +5,15 @@ program.allowUnknownOption(true)
 var fs = require('fs')
 var exec = require('child_process').exec
 
+
 //console.log(process.argv)
+
+var def = require('./lib/Default.js')
+
+program
+  .command('default')
+  .description('sets a raspbian back to default configuration')
+  .action(def)
 
 var reboot = require('./lib/Reboot.js')
 
