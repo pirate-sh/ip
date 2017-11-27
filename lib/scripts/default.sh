f2a3103d80bb84ad503db80f49615e02a0947b5a
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-11-27 17:16:39 -0300

    default stuff

--- lib/scripts/default.sh
+++ lib/scripts/default.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo $1
+cp "$1/templates/network/interfaces/default" "/etc/network/interfaces"
+cp "$1/templates/network/wpa_supplicant" "/etc/wpa_supplicant/wpa_supplicant.conf"
+cp "$1/templates/rc.local/default" "/etc/rc.local"
+rm -rf /etc/hostapd.conf
+pirateship rename raspberrypi
+systemctl disable hostapd || true
+systemctl disable dnsmasq || true
