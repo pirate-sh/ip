3913ef98dc32602181dc134b3e37a2dc7db57301
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-11-27 17:19:17 -0300

    remove interfaces

--- lib/scripts/default.sh
+++ lib/scripts/default.sh
@@ -3,6 +3,7 @@ cp "$1/templates/network/interfaces/default" "/etc/network/interfaces"
 cp "$1/templates/network/wpa_supplicant" "/etc/wpa_supplicant/wpa_supplicant.conf"
 cp "$1/templates/rc.local/default" "/etc/rc.local"
 rm -rf /etc/hostapd.conf
+rm -rf /etc/network/interfaces.d/*
 pirateship rename raspberrypi
 systemctl disable hostapd || true
 systemctl disable dnsmasq || true
