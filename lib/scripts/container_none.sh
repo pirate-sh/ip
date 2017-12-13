98c077af2041a56329010c024a78f6b74c02b8a2
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-12-13 00:38:47 -0300

    Use source for functions

--- lib/scripts/container_none.sh
+++ lib/scripts/container_none.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-function stop_service() {
-  if [ "`systemctl is-active $1`" == "active" ]
-  then
-    systemctl stop $1
-  fi
-}
-
-function disable_service() {
-  if [ "`systemctl is-enabled $1`" == "enabled" ]
-  then
-    systemctl disable $1
-  fi
-}
+source lib/scripts/services_functions.sh
 
 disable_service balena
 disable_service docker
