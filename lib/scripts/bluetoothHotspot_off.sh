e8597cc3f14459e4b96d610af912ecb4b2c9f01e
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-12-29 21:03:58 -0300

    fix pirateship bluetooth off (#39)

--- lib/scripts/bluetoothHotspot_off.sh
+++ lib/scripts/bluetoothHotspot_off.sh
@@ -6,3 +6,6 @@ source $source_dir/services_functions.sh
 disable_service rpibluetooth
 stop_service rpibluetooth
 restart_service bluetooth
+
+sleep 2 # Wait few seconds for bluetooth to start
+restart_service bluealsa # restart the bluetooth audio service
